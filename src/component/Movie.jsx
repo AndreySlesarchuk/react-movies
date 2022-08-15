ac560e04032d7597644c40ed71161801d50f93a7
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-08-15 10:27:40 +0300

    some issues fixed

--- src/component/Movie.jsx
+++ src/component/Movie.jsx
@@ -7,22 +7,22 @@ function Movie(props) {
         Poster: poster
     } = props
 
-    return <div id={id} className="card movie">
-        <div className="card-image waves-effect waves-block waves-light">
+    return <div id={id} className='card movie'>
+        <div className='card-image waves-effect waves-block waves-light'>
             {
                 poster === 'N/A'
-                    ? <img className="activator" src={
-                        `https://via.placeholder.com/300x400?text=${title}`} />
-                    : <img className="activator" src={poster}/>
+                    ? <img className='activator' src={
+                        `https://via.placeholder.com/300x400?text=${title}`} alt='poster'/>
+                    : <img className='activator' src={poster} alt='poster'/>
             }
         </div>
-        <div className="card-content">
-            <span className="card-title activator grey-text text-darken-4">
+        <div className='card-content'>
+            <span className='card-title activator grey-text text-darken-4'>
                 {title}
             </span>
             <p>
                 {year}
-                <span className="right">{type}</span>
+                <span className='right'>{type}</span>
             </p>
         </div>
     </div>
